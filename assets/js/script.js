a1398ffba585f42c9ea1f86ec0f7987f23beac89
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-06-20 15:34:02 +0200

    Create the check answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,14 +26,29 @@ function runGame(gameType) {
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if(gameType === "addition") {
-        displayAdditionQuestion(num1,num2)
+        displayAdditionQuestion(num1,num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
 
+/**
+ * Check if the answer is correct comparing
+ * with the array returned by each operation
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awwww... you answered ${userAnswer}. THe correct answer was ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);
 
 }
 
@@ -63,7 +78,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
